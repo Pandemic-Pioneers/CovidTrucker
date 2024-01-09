e5f6dd4b54ddaa2b6c09cb414c26416e0204abb5
atefa1234 <atefa.mahmoodi2021@gmail.com>
2024-01-09 11:33:30 +0100

    styles of navbar in progress

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 <>
     <div className='navbar'>
         <ul>
-            <li className="icons"><img src="src/assets/images/coronavirus.png" alt="logo" /></li>
+            <li className="logo"><img src="src/assets/images/coronavirus.png" alt="logo" /></li>
             <li className="icons"><img src="src/assets/images/icons/graphic.svg" alt="Graphic" /></li>
             <li className="icons"><img src="src/assets/images/icons/list.svg" alt="List" /></li>
             <li className="icons"><img src="src/assets/images/icons/icons.svg" alt="Icons" /></li>
@@ -17,7 +17,7 @@ const Navbar = () => {
             <li className="icons"><img src="src/assets/images/icons/world.svg" alt="World" /></li>
             <li className="icons"><img src="src/assets/images/icons/coment.svg" alt="Comments" /></li>
             <li className="icons"><img src="src/assets/images/icons/stuff.svg" alt="Stuff" /></li>
-            
+
         </ul>
     </div>
     </>
