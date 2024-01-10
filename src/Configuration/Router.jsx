84fe9b44852d8d16971e6e89c6169a6b3bf83fc1
diana <dialomt@gmail.com>
2024-01-10 13:05:45 +0100

    navbar fixed

--- src/Configuration/Router.jsx
+++ src/Configuration/Router.jsx
@@ -10,11 +10,7 @@ const Router = () => {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Navbar/>}></Route>
-<<<<<<< HEAD
-        <Route path="/tracker0" element={<Trackercero />}></Route>
-=======
         <Route path="/Prueba" element={<Prueba/>}></Route>
->>>>>>> 12e8871061a324b427674e80d9ccf07c2fedaa90
     </Routes>
     </BrowserRouter>
     </>
