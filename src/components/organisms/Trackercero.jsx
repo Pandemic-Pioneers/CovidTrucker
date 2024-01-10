c092010856606b57bfa40f5a45251445e2d8fc5c
islacrur <islacrur@gmail.com>
2024-01-10 16:12:03 +0100

    API Test in trackercero

--- src/components/organisms/Trackercero.jsx
+++ src/components/organisms/Trackercero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './trackercero.css'
 import ColorData from '../atoms/ColorData'
+import CovidData from '../../Covid'
 
 const Trackercero = () => {
   return(
     <>
     <div className='trackercero_background'>
     <h2 className='trackercero_Title'>COVID-19 Tracker</h2>
+    <CovidData />
    <ColorData/></div>
     </>
   )}
