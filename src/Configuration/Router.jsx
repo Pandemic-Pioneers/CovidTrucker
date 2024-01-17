6f8c2f01e256de290995764f459e0fa413993a2e
islacrur <islacrur@gmail.com>
2024-01-17 20:13:27 +0100

    Tracker 1 linked

--- src/Configuration/Router.jsx
+++ src/Configuration/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Trackercero from "../components/organisms/Trackercero";
 import Prueba from "../components/pages/HomePage/Prueba";
-/* import Tracker_1 from "../components/pages/Tracker_1/tracker_1"; */
+import Tracker1 from "../components/pages/Tracker1/Tracker1"
 
 const Router = () => {
   return (
@@ -11,7 +11,7 @@ const Router = () => {
     <Trackercero />
     <BrowserRouter>
     <Routes>
-        {/* <Route path="/tracker_1" element={<Prueba/>}></Route> */}
+      <Route path="/tracker_1" element={<Tracker1 />}></Route>
       
 
     </Routes>
---
+++ src/components/pages/Tracker1/Tracker1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './tracker1.css'
+
+const Tracker1 = () => {
+  return (
+    <div className='stylesTracker1'>Tracker1</div>
+  )
+}
+
+export default Tracker1
